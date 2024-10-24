22be72cfb84d30949d19226a542f4b198d478452
slielsea <samirasaidkamolova@gmail.com>
2024-10-24 15:54:45 +0500

    file

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard
+
+
+      <div>
+        <h1>Admin Dashboard Starter Kit</h1>
+        <ul>
+            <li>✓ Light/dark mode toggle</li>
+            <li>✓ Redux toolkit and other utility libraries configured
+            </li>
+            <li>
+            ✓ Calendar, Modal, Sidebar components
+
+            </li>
+            <li>✓ User-friendly documentation
+            </li>
+            <li>✓ Daisy UI components, Tailwind CSS support
+            </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
